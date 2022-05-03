8086e9b1c00040e89d5a78675df5a253c1fd3f27
Hudson <hudson.junior>
2022-05-03 10:41:56 -0300

    major refactoring

--- main.py
+++ main.py
@@ -1,44 +1,55 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot
-from pandas.plotting import scatter_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import HalvingGridSearchCV
 
 
 def apresentaResultados(name, Y_validacao, predicoes):
-    print("Acurácia " + name + ": " + str(accuracy_score(Y_validacao, predicoes)))
+    print('\n--------------------------- ' +
+          name + ' ---------------------')
 
-    print("\nMatriz de confusão " + name + ": ")
+    # Apresentação da acurácia obtiad dado o modelo.
+    print("Acurácia: " + str(accuracy_score(Y_validacao, predicoes)))
+
+    # Apresentação da matriz de confusão
+    print("\nMatriz de confusão: ")
     print(confusion_matrix(Y_validacao, predicoes))
 
-    print("\nClassificação " + name + ": ")
-    print(classification_report(Y_validacao, predicoes))
+    # Apresentação da precisão, recall, f1-score e support
+    print("\nClassificação: ")
+    print(classification_report(Y_validacao, predicoes, zero_division=1))
+
+    print('--------------------------------------------------------\n')
 
 
-def getPredicoes(modelo, params, X_treino, Y_treino, X_validacao):
-    search_LDA = HalvingGridSearchCV(
-        modelo, params, random_state=0).fit(X_treino, Y_treino)
+def calcularClassificacao(modelo, params, X_treino, Y_treino, X_validacao):
+    # Aplicação da hiperparametrização a partir do:
+    # - Modelo
+    # - Parâmetros definidos
+    # - X_validação -> Para o caso do dataset completo, será 20% da base de teste
+    #               -> Para o caso dos cenários extras, será os 4 cenários de uso de teste
 
-    return search_LDA.predict(X_validacao)
+    hyper_param_model = HalvingGridSearchCV(
+        modelo, params, random_state=0)
+
+    fitted_model = hyper_param_model.fit(X_treino, Y_treino)
+
+    return fitted_model.predict(X_validacao)
 
 
 def main():
+    # Leitura da base de dados dos 7 tipos diferentes de feijão seco.
     base_dados = pd.read_excel(r'dry_bean_dataset.xlsx')
     valores_base_dados = base_dados.values
 
+    # Separação dos cenários de uso extras, utilizados separadamente
+    # para testes.
     index_inicial = 0
     index_meio_1 = int(len(valores_base_dados)/3)
     index_meio_2 = int((len(valores_base_dados)/3) * 2)
@@ -57,42 +68,69 @@ def main():
     X = valores_base_dados[:, 0:16]
     Y = valores_base_dados[:, 16]
 
+    # Divisão do dataset sem os 4 cenários de uso
     X0 = np.array(cenarios_extras[:, 0:16])
     Y0 = np.array(cenarios_extras[:, 16])
 
+    # Divisão do dataset com 80% para treino e 20% para teste
     X_treino, X_validacao, Y_treino, Y_validacao = train_test_split(
         X, Y, test_size=0.2, random_state=1)
 
-    # Modelos que avaliaremos para escolher entre os dois melhores
+    # Modelos utilizados para treinanamento.
     modelo_LDA = LinearDiscriminantAnalysis()
     modelo_CART = DecisionTreeClassifier()
-    modelo_SVC = SVC()
 
+    # Parâmetrização do LDA para aplicação do GridSearch.
     param_grid_LDA = {"solver": ['svd', 'lsqr', 'eigen']}
 
-    predicoes_LDA = getPredicoes(
-        modelo_LDA, param_grid_LDA, X_treino, Y_treino, X_validacao)
+    # Calculando classificação do modelo utilizando LDA e dataset de treino com 80%
+    # e dataset de teste com 20%
+    predicoes_LDA = calcularClassificacao(
+        modelo_LDA, param_grid_LDA,
+        X_treino, Y_treino, X_validacao
+    )
 
-    predicoes_LDA_extra = getPredicoes(
-        modelo_LDA, param_grid_LDA, X_treino, Y_treino, X0)
+    # Calculando classificação do modelo utilizando LDA e dataset
+    # sem os 4 cenários de uso definidos
+    predicoes_LDA_extra = calcularClassificacao(
+        modelo_LDA, param_grid_LDA,
+        X_treino, Y_treino, X0
+    )
 
-    apresentaResultados('LDA 0', Y0, predicoes_LDA_extra)
+    # Apresentando os resultados obtidos para os dois casos acima
+    # utilizando o algoritmo LDA.
+    apresentaResultados('LDA - Dataset completo', Y_validacao, predicoes_LDA)
+    apresentaResultados('LDA - 4 cenários de uso', Y0, predicoes_LDA_extra)
 
-    # param_grid_CART = {"criterion": [
-    #     "gini", "entropy"], "splitter": ["best", "random"]}
-    #
-    # predicoes_CART = getPredicoes(
-    #     modelo_CART, param_grid_CART, X_validacao, X_treino, Y_treino)
+    # Parâmetrização do CART para aplicação do GridSearch.
+    param_grid_CART = {"criterion": [
+        "gini", "entropy"], "splitter": ["best", "random"]}
 
-    # param_grid_SVC = {"kernel": [
-    #   "linear", "poly", "sigmoid", "precomputed"], "C": [pow(10, 5), pow(10, 6), pow(10, 7)]}
+    # Calculando classificação do modelo utilizando CART e dataset de treino com 80%
+    # e dataset de teste com 20%
+    predicoes_CART = calcularClassificacao(
+        modelo_CART, param_grid_CART,
+        X_treino, Y_treino, X_validacao
+    )
 
-    # predicoes_SVC = getPredicoes(
-    #   modelo_SVC, param_grid_SVC, X_validacao, X_treino, Y_treino)
+    # Calculando classificação do modelo utilizando CART e dataset
+    # sem os 4 cenários de uso definidos
+    predicoes_CART_extra = calcularClassificacao(
+        modelo_CART, param_grid_CART,
+        X_treino, Y_treino, X0
+    )
 
-    #apresentaResultados('LDA', Y_validacao, predicoes_LDA)
-    #apresentaResultados('CART', Y_validacao, predicoes_CART)
-    #apresentaResultados('SVC', Y_validacao, predicoes_SVC)
+    # Apresentando os resultados obtidos para os dois casos acima
+    # utilizando o algoritmo CART.
+    apresentaResultados('CART - Dataset completo',
+                        Y_validacao, predicoes_CART
+                        )
+
+    apresentaResultados('CART - 4 cenários de uso',
+                        Y0, predicoes_CART_extra
+                        )
 
 
-main()
+# Chamanda principal do programa.
+if __name__ == '__main__':
+    main()
